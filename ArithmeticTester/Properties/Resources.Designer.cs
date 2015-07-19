31485a9a413ac2a7d04bfcd0f70aab6f3b505147
wiganlatics <wiganlatics@users.noreply.github.com>
2015-07-19 18:27:12 +0100

    Using Resources files
    1) Remove unused form resx files
    2) Add strings to Resources.resx file
    3) Use a constant for number of questions
    4) Use the resx strings rather than hardcoded ones
    5) Use specific functions for updating UI elements

--- ArithmeticTester/Properties/Resources.Designer.cs
+++ ArithmeticTester/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ArithmeticTester.Properties
-{
-
-
+namespace ArithmeticTester.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,176 @@ namespace ArithmeticTester.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ArithmeticTester.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to That is not a number!.
+        /// </summary>
+        internal static string AnswerNotANumberMessage {
+            get {
+                return ResourceManager.GetString("AnswerNotANumberMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Correct!.
+        /// </summary>
+        internal static string CorrectAnswerMessage {
+            get {
+                return ResourceManager.GetString("CorrectAnswerMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Correct.
+        /// </summary>
+        internal static string CorrectLabel {
+            get {
+                return ResourceManager.GetString("CorrectLabel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Excellent.
+        /// </summary>
+        internal static string ExcellentGrade {
+            get {
+                return ResourceManager.GetString("ExcellentGrade", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Good.
+        /// </summary>
+        internal static string GoodGrade {
+            get {
+                return ResourceManager.GetString("GoodGrade", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Guesses.
+        /// </summary>
+        internal static string GuessesLabel {
+            get {
+                return ResourceManager.GetString("GuessesLabel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Incorrect, you are {0} away!.
+        /// </summary>
+        internal static string IncorrectGuessMessage {
+            get {
+                return ResourceManager.GetString("IncorrectGuessMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Incorrect, it was {0}.
+        /// </summary>
+        internal static string IncorrectThirdGuessMessage {
+            get {
+                return ResourceManager.GetString("IncorrectThirdGuessMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Arithmetic Tester.
+        /// </summary>
+        internal static string MessageBoxTitle {
+            get {
+                return ResourceManager.GetString("MessageBoxTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Outstanding.
+        /// </summary>
+        internal static string OutstandingGrade {
+            get {
+                return ResourceManager.GetString("OutstandingGrade", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Question.
+        /// </summary>
+        internal static string QuestionLabel {
+            get {
+                return ResourceManager.GetString("QuestionLabel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Satisfactory.
+        /// </summary>
+        internal static string SatisfactoryGrade {
+            get {
+                return ResourceManager.GetString("SatisfactoryGrade", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to You have completed the test.
+        /// </summary>
+        internal static string TestCompleteMessage {
+            get {
+                return ResourceManager.GetString("TestCompleteMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Unknown Arithmetic Operator..
+        /// </summary>
+        internal static string UnknownOperatorException {
+            get {
+                return ResourceManager.GetString("UnknownOperatorException", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Please see your teacher.
+        /// </summary>
+        internal static string UnsatisfactoryGrade {
+            get {
+                return ResourceManager.GetString("UnsatisfactoryGrade", resourceCulture);
             }
         }
     }
