8c94ab1d879b002f7ee72eb0cc7796557fb2c5bd
wiganlatics <wiganlatics@users.noreply.github.com>
2015-07-20 20:42:37 +0100

    Populate datagrid on answers form
    1) Make frmAnswerTable un-sizable and position in centre of screen
    2) Populate the answer table data grid
    3) Fit answers table data grid to show all data and fill the form
    4) Make data grid un-sizable and un-editable

--- ArithmeticTester/Views/frmAnswersTable.Designer.cs
+++ ArithmeticTester/Views/frmAnswersTable.Designer.cs
@@ -36,22 +36,33 @@
             // 
             this.grdAnswersTable.AllowUserToAddRows = false;
             this.grdAnswersTable.AllowUserToDeleteRows = false;
+            this.grdAnswersTable.AllowUserToResizeColumns = false;
+            this.grdAnswersTable.AllowUserToResizeRows = false;
+            this.grdAnswersTable.BackgroundColor = System.Drawing.SystemColors.Control;
             this.grdAnswersTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdAnswersTable.Enabled = false;
+            this.grdAnswersTable.GridColor = System.Drawing.SystemColors.Control;
             this.grdAnswersTable.Location = new System.Drawing.Point(0, 0);
+            this.grdAnswersTable.MultiSelect = false;
             this.grdAnswersTable.Name = "grdAnswersTable";
             this.grdAnswersTable.ReadOnly = true;
-            this.grdAnswersTable.Size = new System.Drawing.Size(240, 150);
+            this.grdAnswersTable.RowHeadersWidth = 50;
+            this.grdAnswersTable.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.grdAnswersTable.Size = new System.Drawing.Size(412, 287);
             this.grdAnswersTable.TabIndex = 0;
             // 
             // frmAnswersTable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(412, 287);
             this.Controls.Add(this.grdAnswersTable);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmAnswersTable";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "AnswersTable";
-            this.Load += new System.EventHandler(this.frmAnswersTable_Load);
             ((System.ComponentModel.ISupportInitialize)(this.grdAnswersTable)).EndInit();
             this.ResumeLayout(false);
 
--- ArithmeticTester/Views/frmAnswersTable.cs
+++ ArithmeticTester/Views/frmAnswersTable.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using ArithmeticTester.Classes;
 
 namespace ArithmeticTester.Views
 {
@@ -15,35 +9,47 @@ namespace ArithmeticTester.Views
     public partial class frmAnswersTable : Form
     {
         /// <summary>
-        /// 
+        /// The minimum factor to use in arithmetic operations.
         /// </summary>
         private const byte minFactorValue = 1;
         /// <summary>
-        /// 
+        /// The maximum factor to use in arithmetic operations.
         /// </summary>
         private const byte maxFactorValue = 12;
 
         /// <summary>
-        /// 
+        /// Form Constructor.
         /// </summary>
         public frmAnswersTable()
         {
             InitializeComponent();
+
+            Initialise();
         }
 
         /// <summary>
-        /// 
+        /// Initialise the form.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void frmAnswersTable_Load(object sender, EventArgs e)
+        private void Initialise()
         {
-            for (byte b = 1; b <= 12; b++)
+            for (byte b = minFactorValue; b <= maxFactorValue; b++)
             {
+                grdAnswersTable.Columns.Add(b.ToString(), b.ToString());
+                grdAnswersTable.Columns[b - 1].Width = 30;
                 grdAnswersTable.Rows.Add();
-                grdAnswersTable.Columns.Add(b.ToString(),b.ToString());
-                grdAnswersTable[b, 0].Value = b;
+                grdAnswersTable.Rows[b - 1].HeaderCell.Value = b.ToString();
+
+                // Populate cells
+                for (byte c = minFactorValue; c < b; c++)
+                {
+                    grdAnswersTable[c - 1, b - 1].Value = Arithmetic.Multiply(c, b);
+                    grdAnswersTable[b - 1, c - 1].Value = grdAnswersTable[c - 1, b - 1].Value;
+                }
+                grdAnswersTable[b - 1, b - 1].Value = Arithmetic.Multiply(b, b);
             }
+
+            // Don't want any cells highlighted
+            grdAnswersTable.ClearSelection();
         }
     }
 }
